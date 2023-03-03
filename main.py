5b4a35e5fe8819ee63905645644b7a144623b18e
Happydeath97 <98758077+Happydeath97@users.noreply.github.com>
2023-03-03 11:13:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scrap_google import get_links
 
 
 app = Flask(__name__)
+port = int(os.environ.get('PORT', 5000))
 
 
 @app.route('/')
@@ -16,3 +17,7 @@ def get_searching_term():
 
     links = get_links(search_term)
     return render_template('index.html', links=links)
+
+if __name__ == '__main__':
+    # Run the app on the specified port
+    app.run(host='0.0.0.0', port=port)
